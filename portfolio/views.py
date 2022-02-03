e97d56a8f467e1d63077aff20ca6cbd9de335ce1
bogdansizon <bogdansizon@yandex.ru>
2022-02-03 16:01:44 +0300

    First commit

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,3 +5,5 @@ from .models import Project # Импортируем модель проекта
 def home(request):
     projects = Project.objects.all() # Импортируем все записи о проектах из базы данных
     return render(request, 'portfolio/home.html', {'projects':projects} ) # Используя переменную projects, передаем словарь в шаблом с ключем projects и значением: список projects выше
+def about(request):
+    return render (request, 'portfolio/about.html')
